63435bf72e358a254287c2065d5e70dd0c006af9
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-03 10:45:00 +0100

    Updated age placeholder

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Fetch all the values of the inputs and store in individual variables
+
+const firstName = document.querySelector('#fname').nodeValue;
+const submit = document.querySelector('.btn');
+
+submit.addEventListener('onsubmit', function (evt) {
+  evt.preventDefault();
+  window.history.back();
+});
+submit.addEventListener('click', (e) => {
+  e.defaultPrevented();
+  console.log(firstName);
+});
