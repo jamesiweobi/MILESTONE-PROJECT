b866fc0a382b6add1e149cf916b7fad1cd820068
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-04 14:35:52 +0100

    Added the table populating logic

--- app.js
+++ app.js
@@ -10,36 +10,30 @@ const submit = document.querySelector('#btn');
 const table = document.querySelector('#table');
 const newTableRow = document.querySelector('#table tbody');
 
-const getData = (firstName, surname, age, select, club) => {
-  const newPerson = {
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  table.classList.add('show-table');
+  let userData = {
     firstName: firstName.value,
     surname: surname.value,
     age: age.value,
     select: select.value,
     club: club.value,
   };
-  return newPerson;
-};
-const data = getData(firstName, surname, age, select, club);
-const tableContent = `
-   <tr>
-        <td>${data.firstName}</td>
-        <td>${data.surname}</td>
-        <td>${data.age}</td>
-        <td>${data.select}</td>
-        <td>${data.club}</td>
+
+  let tableContent = ` <tr>
+        <td>${userData.firstName}</td>
+        <td>${userData.surname}</td>
+        <td>${userData.age}</td>
+        <td>${userData.select}</td>
+        <td>${userData.club}</td>
         <td>
-          <input class="btn-close"><i class="fas fa-times close"></i></input>
+          <button class="delete">Delete</button>
         </td>
       </tr>
 
 `;
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  table.classList.add('show-table');
 
-  console.log(tableContent);
-  // newTableRow.
-
-  console.log(data);
+  newTableRow.insertAdjacentHTML('beforeend', tableContent);
+  console.log(userData);
 });
