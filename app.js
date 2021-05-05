dc50b208cf4868bcdb68011d7073f04674939a60
jamesiweobi <60060852+jamesiweobi@users.noreply.github.com>
2021-05-05 15:19:26 +0100

    Completed the functionality for the form and delete button

--- app.js
+++ app.js
@@ -9,28 +9,12 @@ const club = document.querySelector('input[name = "club"]');
 const submit = document.querySelector('#btn');
 const table = document.querySelector('#table');
 const newTableRow = document.querySelector('#table tbody');
-
+// Add EventListener to the submit btn, prevent the default behaviour on submit.
 submit.addEventListener('click', (e) => {
   e.preventDefault();
+  // Made the table visible in the html
   table.classList.add('show-table');
-  newTableRow.insertAdjacentHTML('beforeend', prepUser());
-  console.log(myJson);
-});
-
-const getUserData = (firstName, surname, age, select, club) => {
-  const userData = {
-    firstName: firstName.value,
-    surname: surname.value,
-    age: age.value,
-    select: select.value,
-    club: club.value,
-  };
-  return userData;
-};
-
-const myJson = [];
-myJson.push(userData);
-const prepUser = () => {
+  // fetched user input and inserted it into the HTML and then reset the form right after
   const user = getUserData(firstName, surname, age, select, club);
   let tableContent = ` <tr>
         <td>${user.firstName}</td>
@@ -43,5 +27,25 @@ const prepUser = () => {
         </td>
       </tr>
 `;
-  return tableContent;
+  newTableRow.insertAdjacentHTML('beforeend', tableContent);
+  form.reset();
+  // The logic for the delete button
+  const deleteBtns = document.querySelectorAll('.delete');
+  deleteBtns.forEach((deleteBtn) => {
+    deleteBtn.addEventListener('click', (e) => {
+      const toDelete = e.currentTarget.parentElement.parentElement;
+      toDelete.remove();
+    });
+  });
+});
+// User object getting function
+const getUserData = (firstName, surname, age, select, club) => {
+  let userData = {
+    firstName: firstName.value,
+    surname: surname.value,
+    age: age.value,
+    select: select.value,
+    club: club.value,
+  };
+  return userData;
 };
